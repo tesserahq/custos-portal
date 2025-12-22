99a9f25b71d8e365b775a62cf578c07880f22d5c
yayakyogi <yayaktaka@gmail.com>
2025-12-22 11:05:01 +0700

    remove unused

--- app/components/form/index.ts
+++ app/components/form/index.ts
@@ -9,8 +9,6 @@ import { FormTextarea } from './form-textarea'
 import { FormEmail } from './form-email'
 import { FormPhoneInput } from './form-phone-input'
 import { FormCommand } from './form-command'
-import { FormContacts } from './form-contacts'
-import { FormInteractionActions } from './form-interaction-actions'
 
 const FormCompound = Object.assign(Form, {
   Input: FormInput,
@@ -22,8 +20,6 @@ const FormCompound = Object.assign(Form, {
   Email: FormEmail,
   PhoneInput: FormPhoneInput,
   Command: FormCommand,
-  Contacts: FormContacts,
-  InteractionActions: FormInteractionActions,
   Provider: FormProvider,
 })
 
